6061cedcab68cd24360fad50bc2162f3d9b273c8
Frank Cairampoma <ingfralch@gmail.com>
2024-04-10 11:43:04 -0500

    arreglando issue

--- src/wmain.js
+++ src/wmain.js
@@ -13,13 +13,42 @@ async function launchBrowser() {
 }
 
 // Objeto que representa el cliente de WhatsApp
-const whatsappClient = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: ["--no-sandbox"]
-    }
-});
+    // const client = new Client({
+    //     webVersionCache: {
+    //         type: 'remote',
+    //         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2410.1.html',
+    //         },
+    //     authStrategy: new LocalAuth(),
+    //     puppeteer: {
+    //         headless: true,
+    //         args: ["--no-sandbox"]
+    //     }
+    // });
+
+
+    // const client = new Client({
+    //     webVersionCache: {
+    //         type: 'remote',
+    //         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2402.5.html',
+    //         },
+    //     authStrategy: new LocalAuth(),
+    //     puppeteer: {
+    //         headless: true,
+    //         args: ["--no-sandbox"]
+    //     }
+    // });
+
+    const client = new Client({
+        webVersionCache: {
+            type: 'remote',
+            remotePath: 'https://raw.githubusercontent.com/fralch/CrediWhatsapp-connect/main/wcache.html',
+            },
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            headless: true,
+            args: ["--no-sandbox"]
+        }
+    });
 
 // Evento para generar y guardar un código QR
 whatsappClient.on('qr', async qr => {
